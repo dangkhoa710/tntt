f63ffca961b21c06c71dd8cc7f148a52ad1895c0
Dang Khoa <khoahnd@irtech.com.vn>
2021-05-13 22:37:20 +0700

    new project

---
+++ Modules/Admin/Resources/views/layouts/header.blade.php
@@ -0,0 +1,136 @@
+<div class="navbar" >
+    <div class="navbar-inner" style="background-color: #0B90C4;">
+        <div class="container-fluid">
+            <a class="btn btn-navbar" data-toggle="collapse" data-target=".top-nav.nav-collapse,.sidebar-nav.nav-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </a>
+            <a class="brand" href="index.html"><span>3T ORIGINAL</span></a>
+
+            <!-- start: Header Menu -->
+            <div class="nav-no-collapse header-nav">
+                <ul class="nav pull-right">
+                    <li class="dropdown hidden-phone">
+                        <a class="btn dropdown-toggle" data-toggle="dropdown" href="#">
+                            <i class="icon-bell"></i>
+                            <span> Thông báo</span>
+                            <span class="label label-important"> 3 </span>
+                        </a>
+                        <ul class="dropdown-menu notifications">
+                            <li class="dropdown-menu-title">
+                                <span>Bạn có 2 thông báo mới</span>
+                                <a href="#refresh"><i class="icon-repeat"></i></a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span class="icon blue"><i class="icon-user"></i></span>
+                                    <span class="message">New user registration</span>
+                                    <span class="time">1 min</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <!-- start: Notifications Dropdown -->
+                    <li class="dropdown hidden-phone">
+                        <a class="btn dropdown-toggle" data-toggle="dropdown" href="#">
+                            <i class="icon-calendar"></i>
+                            <span> Công việc</span>
+                            <span class="label label-important"> 3 </span>
+                        </a>
+                        <ul class="dropdown-menu tasks">
+                            <li class="dropdown-menu-title">
+                                <span>Bạn có 2 công việc</span>
+                                <a href="#refresh"><i class="icon-repeat"></i></a>
+                            </li>
+                            <li>
+                                <a href="#">
+										<span class="header">
+											<span class="title">iOS Development</span>
+											<span class="percent"></span>
+										</span>
+                                    <div class="taskProgress progressSlim red">80</div>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+										<span class="header">
+											<span class="title">Android Development</span>
+											<span class="percent"></span>
+										</span>
+                                    <div class="taskProgress progressSlim green">47</div>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <!-- end: Notifications Dropdown -->
+                    <!-- start: Message Dropdown -->
+                    <li class="dropdown hidden-phone">
+                        <a class="btn dropdown-toggle" data-toggle="dropdown" href="#">
+                            <i class="icon-envelope"></i>
+                            <span> Tin nhắn</span>
+                            <span class="label label-important"> 3 </span>
+                        </a>
+                        <ul class="dropdown-menu messages">
+                            <li class="dropdown-menu-title">
+                                <span>Bạn có 2 tin nhắn chưa xem</span>
+                                <a href="#refresh"><i class="icon-repeat"></i></a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span class="avatar"><img src="{{asset('a/avatar/30.jpg')}}" alt="Avatar"></span>
+                                    <span class="header">
+											<span class="from">
+										    	Dennis Ji
+										     </span>
+											<span class="time">
+										    	6 min
+										    </span>
+										</span>
+                                    <span class="message">
+                                            Lorem ipsum dolor sit amet consectetur adipiscing elit, et al commore
+                                        </span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span class="avatar"><img src="{{asset('a/avatar/30.jpg')}}" alt="Avatar"></span>
+                                    <span class="header">
+											<span class="from">
+										    	Dennis Ji
+										     </span>
+											<span class="time">
+										    	56 min
+										    </span>
+										</span>
+                                    <span class="message">
+                                            Lorem ipsum dolor sit amet consectetur adipiscing elit, et al commore
+                                        </span>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+
+                    <!-- start: User Dropdown -->
+                    <li class="dropdown">
+                        <span class="avatar"><img src="{{asset('a/avatar/30.jpg')}}" alt="Avatar"  style="border-radius:50%;height:50px"></span>
+                        <a class="btn dropdown-toggle" data-toggle="dropdown" href="#">
+                            <i class="halflings-icon white user"></i> Khoa
+                            <span class="caret"></span>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li class="dropdown-menu-title">
+                                <span>Cài đặt tài khoản</span>
+                            </li>
+                            <li><a href="#"><i class="halflings-icon user"></i> Thông tin</a></li>
+                            <li><a href="login.html"><i class="halflings-icon off"></i> Đăng xuất</a></li>
+                        </ul>
+                    </li>
+                    <!-- end: User Dropdown -->
+                </ul>
+            </div>
+            <!-- end: Header Menu -->
+
+        </div>
+    </div>
+</div>
